b98a9e9cc4ad3b981cae811f600e515e2a1313ba
Yusuke Osawa <sawa.bb.pro@gmail.com>
2025-10-22 23:00:31 +0900

    #2 カレンダー表示

--- laravel/resources/views/training/index.blade.php
+++ laravel/resources/views/training/index.blade.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="{{ asset('css/training.css') }}">
 </head>
 <body>
+    <script src="{{ asset('js/lib/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('js/training_top.js') }}"></script>
+
     <header class="header">
         <h1>Physilog</h1>
         <nav>
@@ -17,9 +20,9 @@
     <main class="main">
         <section class="calendar-section">
             <div class="month-header">
-                <button>&lt;</button>
+                <button id="prev-month" value=1>&lt;</button>
                 <h2>2025年10月</h2>
-                <button>&gt;</button>
+                <button id="next-month">&gt;</button>
             </div>
             <table class="calendar">
                 <thead>
@@ -29,16 +32,22 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td></td><td></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                     </tr>
                     <tr>
-                        <td>6</td><td>7</td><td>8</td><td>9</td><td class="today">10</td><td>11</td><td>12</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                     </tr>
                     <tr>
-                        <td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                     </tr>
                     <tr>
-                        <td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                     </tr>
                 </tbody>
             </table>
