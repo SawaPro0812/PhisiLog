b956e88efee97b14410648d270093db1aea65707
Yusuke Osawa <sawa.bb.pro@gmail.com>
2025-10-24 20:39:40 +0900

    #6 種目入力モーダル表示

--- laravel/app/Http/Controllers/TrainingController.php
+++ laravel/app/Http/Controllers/TrainingController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Services\CommonService;
+use Illuminate\Support\Facades\Auth;
 
 class TrainingController extends Controller
 {
+    protected $service;
+
+    public function __construct(CommonService $service)
+    {
+        $this->service = $service;
+    }
+
     /**
      * トップメニュー表示
      */
     public function index(Request $request): View
     {
-        $test = 'test';
+        $userId = Auth::id();
+        $data = $this->service->getTopData($userId);
+        
         return view('training.index', [
-            'user' => $test,
+            'data' => $data,
         ]);
     }
 }
--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <a href="http://localhost:3000/top">トップメニュー</a>
+                    <a href="/top">トップメニュー</a>
                 </div>
             </div>
         </div>
